20bb8d1877d0e3d86df633c38b292ff5a934e9a7
Daniel Lin <ephemient@gmail.com>
2025-12-05 01:04:57 +0800

    Day 4: Use Cell to avoid copy

--- rs/src/day4.rs
+++ rs/src/day4.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::collections::{HashMap, hash_map::Entry};
 
 pub fn solve(input: &str) -> (usize, usize) {
@@ -5,19 +6,26 @@ pub fn solve(input: &str) -> (usize, usize) {
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
-            line.char_indices()
-                .filter_map(move |(x, c)| if c == '@' { Some(((x, y), -1)) } else { None })
+            line.char_indices().filter_map(move |(x, c)| {
+                if c == '@' {
+                    Some(((x, y), Cell::new(-1)))
+                } else {
+                    None
+                }
+            })
         })
         .collect::<HashMap<_, _>>();
-    for (x, y) in points.keys().copied().collect::<Vec<_>>() {
+    for (x, y) in points.keys() {
         for x in x.saturating_sub(1)..=x.saturating_add(1) {
             for y in y.saturating_sub(1)..=y.saturating_add(1) {
-                points.entry((x, y)).and_modify(|count| *count += 1);
+                if let Some(cell) = points.get(&(x, y)) {
+                    cell.update(|count| count + 1);
+                }
             }
         }
     }
     let mut removals = points
-        .extract_if(|_, count| *count < 4)
+        .extract_if(|_, count| count.get() < 4)
         .map(|(point, _)| point)
         .collect::<Vec<_>>();
     let (part1, mut part2) = (removals.len(), 0);
@@ -26,9 +34,9 @@ pub fn solve(input: &str) -> (usize, usize) {
         for x in x.saturating_sub(1)..=x.saturating_add(1) {
             for y in y.saturating_sub(1)..=y.saturating_add(1) {
                 match points.entry((x, y)) {
-                    Entry::Occupied(mut entry) => {
-                        *entry.get_mut() -= 1;
-                        if *entry.get() < 4 {
+                    Entry::Occupied(entry) => {
+                        entry.get().update(|count| count - 1);
+                        if entry.get().get() < 4 {
                             removals.push(entry.remove_entry().0);
                         }
                     }
