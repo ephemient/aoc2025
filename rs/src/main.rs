afce92f0c0534588b48ad3f75419237ee298c4f0
Daniel Lin <ephemient@gmail.com>
2025-12-09 03:59:16 +0800

    Better printing

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -19,59 +19,72 @@ fn main() -> io::Result<()> {
     if args.is_empty() || args.contains("1") {
         println!("Day 1");
         let input = get_day_input(1)?;
-        println!("{:?}", day1::solve(&input));
+        let (part1, part2) = day1::solve(&input);
+        println!("{}", part1);
+        println!("{}", part2);
         println!();
     }
 
     if args.is_empty() || args.contains("2") {
         println!("Day 2");
         let input = get_day_input(2)?;
-        println!("{:?}", day2::part1(&input));
-        println!("{:?}", day2::part2(&input));
+        println!("{}", day2::part1(&input));
+        println!("{}", day2::part2(&input));
         println!();
     }
 
     if args.is_empty() || args.contains("3") {
         println!("Day 3");
         let input = get_day_input(3)?;
-        println!("{:?}", day3::part1(&input));
-        println!("{:?}", day3::part2(&input));
+        println!("{}", day3::part1(&input));
+        println!("{}", day3::part2(&input));
         println!();
     }
 
     if args.is_empty() || args.contains("4") {
         println!("Day 4");
         let input = get_day_input(4)?;
-        println!("{:?}", day4::solve(&input));
+        let (part1, part2) = day4::solve(&input);
+        println!("{}", part1);
+        println!("{}", part2);
         println!();
     }
 
     if args.is_empty() || args.contains("5") {
         println!("Day 5");
         let input = get_day_input(5)?;
-        println!("{:?}", day5::solve(&input));
+        let (part1, part2) = day5::solve(&input);
+        println!("{}", part1);
+        println!("{}", part2);
         println!();
     }
 
     if args.is_empty() || args.contains("6") {
         println!("Day 6");
         let input = get_day_input(6)?;
-        println!("{:?}", day6::part1(&input));
-        println!("{:?}", day6::part2(&input));
+        println!("{}", day6::part1(&input));
+        println!("{}", day6::part2(&input));
         println!();
     }
 
     if args.is_empty() || args.contains("7") {
         println!("Day 7");
         let input = get_day_input(7)?;
-        println!("{:?}", day7::solve(&input));
+        let (part1, part2) = day7::solve(&input);
+        println!("{}", part1);
+        println!("{}", part2);
         println!();
     }
 
     if args.is_empty() || args.contains("8") {
         println!("Day 8");
         let input = get_day_input(8)?;
-        println!("{:?}", day8::solve(&input, 1000));
+        let (part1, part2) = day8::solve(&input, 1000);
+        println!("{}", part1);
+        println!(
+            "{}",
+            part2.map_or_else(|| "null".to_string(), |part2| part2.to_string())
+        );
         println!();
     }
 
