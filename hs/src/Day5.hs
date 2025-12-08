1043c83ad02a086a5e45a2866d1ff38259fc1129
Daniel Lin <ephemient@gmail.com>
2025-12-09 03:41:41 +0800

    Day 5: Rewrite with Vector

--- hs/src/Day5.hs
+++ hs/src/Day5.hs
@@ -2,37 +2,46 @@
 
 module Day5 (day5) where
 
-import Data.Ix (rangeSize)
-import Data.Map qualified as Map (assocs, empty, lookupLE, lookupMax, lookupMin, singleton)
-import Data.Map.Extra qualified as Map (spanAntitoneWithElem3)
+import Control.Arrow (second)
+import Control.Monad.ST (runST)
+import Data.Monoid (Sum (Sum, getSum))
 import Data.Text (Text)
 import Data.Text qualified as T (lines, null, pattern Empty, pattern (:<))
 import Data.Text.Read qualified as T (decimal)
+import Data.Vector.Algorithms.Intro qualified as MV (sort)
+import Data.Vector.Algorithms.Search qualified as MV (binarySearchP, binarySearchPBounds)
+import Data.Vector.Unboxed qualified as V (create, foldMap', fromList, thaw, unsafeThaw, (!?))
+import Data.Vector.Unboxed.Mutable qualified as MV (foldM', modify, take, write)
 
 day5 :: Text -> (Int, Int)
 day5 input =
   ( length
       [ ()
       | line <- input2,
-        (a, T.Empty) <- either (const mempty) pure $ T.decimal @Int line,
-        maybe False ((a <=) . snd) $ Map.lookupLE a ranges
+        (x, T.Empty) <- decimal line,
+        maybe False ((<= x) . fst) $
+          ranges V.!? runST (V.unsafeThaw ranges >>= MV.binarySearchP ((x <=) . snd))
       ],
-    sum $ rangeSize <$> Map.assocs ranges
+    getSum $ V.foldMap' (Sum . succ . uncurry subtract) ranges
   )
   where
     (input1, input2) = break T.null $ T.lines input
-    ranges = foldl' addRange Map.empty input1
-    addRange ranges line
-      | Right (lo, '-' T.:< rest) <- T.decimal line,
-        Right (hi, T.Empty) <- T.decimal rest,
-        (rangesLT, rangesEQ, rangesGT) <- Map.spanAntitoneWithElem3 (cmp lo hi) ranges =
-          rangesLT
-            <> Map.singleton
-              (maybe lo (min lo . fst) $ Map.lookupMin rangesEQ)
-              (maybe hi (max hi . snd) $ Map.lookupMax rangesEQ)
-            <> rangesGT
-      | otherwise = ranges
-      where
-        cmp lo _ _ hi | hi < lo = LT
-        cmp _ hi lo _ | hi < lo = GT
-        cmp _ _ _ _ = EQ
+    ranges = V.create $ do
+      ranges <-
+        V.thaw $
+          V.fromList
+            [ (lo, hi)
+            | line <- input1,
+              (lo, '-' T.:< line') <- decimal line,
+              (hi, T.Empty) <- decimal line'
+            ]
+      MV.sort ranges
+      let go count range@(lo, hi) = do
+            i <- MV.binarySearchPBounds ((lo <=) . snd) ranges 0 count
+            if i < count
+              then MV.modify ranges (second $ max hi) i
+              else MV.write ranges i range
+            pure $ i + 1
+      count <- MV.foldM' go 0 ranges
+      pure $ MV.take count ranges
+    decimal = either (const mempty) pure . T.decimal
